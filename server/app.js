66c4d9a5551e45e7e750d483e6dec30074236e9d
username <cshivam3312@gmail.com>
2022-03-17 12:58:33 +0530

    Initial front-end commit

--- server/app.js
+++ server/app.js
@@ -35,6 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 //user middleware
 
