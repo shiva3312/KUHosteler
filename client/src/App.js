d17a9c3d7a77e37ecda912fe08e1ba1561ab6b96
Jukta Maitra <www.ajstsf@gmail.com>
2022-03-17 13:01:28 +0530

    signup & login components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import RegisterStudent from './components/RegisterStudent';
+import RegisterManager from './components/RegisterManager';
+import RegisterStaff from './components/RegisterStaff';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RegisterStaff/>
+      <Login/>
+      <RegisterManager/>;
+      <RegisterStudent/>;
+    
     </div>
   );
 }
